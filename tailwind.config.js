1098b28009283b25f4c3333f27cbdba40919c369
joeymariano <joey.michalina.mariano@gmail.com>
2024-12-18 14:26:15 -0500

    tailwind up and running - proper templates added for jekyll

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./**/*.html", // Target your HTML files
+    "./**/*.js",   // Target your JS files
+    // You can add more paths like this if needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
