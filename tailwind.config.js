505adf78b71136db4f25f4c31b25b8a3af82f152
joeymariano <joey.michalina.mariano@gmail.com>
2024-12-20 16:10:17 -0500

    tailwind config conflict gone

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: [
-    "./**/*.html", // Target your HTML files
-    "./**/*.js",   // Target your JS files
+    "./_layouts/default.html", // Target your HTML files
+    "./_layouts/home.html", // Target your HTML files
+    "./_layouts/page.html", // Target your HTML files
+    "./_layouts/post.html", // Target your HTML files
+    "./_includes/footer.html", // Target your HTML files
+    "./_includes/head.html", // Target your HTML files
+    "./_includes/header.html", // Target your HTML files
+    "./assets/js/scripts.js",   // Target your JS files
     // You can add more paths like this if needed
   ],
   theme: {
