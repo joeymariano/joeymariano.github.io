505eb4ccdbe13ed960fa8ac63da6d86eea2c90fc
joeymariano <joey.michalina.mariano@gmail.com>
2025-03-19 15:24:29 -0400

    refactoring pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   content: [
     "./_layouts/default.html", // Target your HTML files
     "./_layouts/home.html", // Target your HTML files
-    "./_layouts/page.html", // Target your HTML files
+    "./_layouts/resume.html", // Target your HTML files
     "./_layouts/post.html", // Target your HTML files
     "./_includes/footer.html", // Target your HTML files
     "./_includes/head.html", // Target your HTML files
