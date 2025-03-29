4278d338a786395af80f7a96f4418fcd8531731b
joeymariano <joey.michalina.mariano@gmail.com>
2025-03-29 16:25:33 -0400

    added smooth scroll to left right buttons

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,16 +1,26 @@
 // navbar script
 
 document.addEventListener('DOMContentLoaded', () => {
-  const menuBtn = document.getElementById('menu-btn');
-  const menu = document.getElementById('menu');
+    const menuBtn = document.getElementById('menu-btn');
+    const menu = document.getElementById('menu');
+    const jobBtnBack = document.getElementById('job-btn-back');
+    const jobBtnForward = document.getElementById('job-btn-forward');
+    const jobScroller = document.getElementById('job-scroller');
 
-  menuBtn.addEventListener('click', () => {
-    if (menu.classList.contains('hidden')) {
-      menu.classList.remove('hidden');
-      setTimeout(() => menu.classList.add('visible'), 10); // Short delay to trigger transition
-    } else {
-      menu.classList.remove('visible');
-      setTimeout(() => menu.classList.add('hidden'), 500); // Matches transition duration
-    }
-  });
+    menuBtn.addEventListener('click', () => {
+        if (menu.classList.contains('hidden')) {
+            menu.classList.remove('hidden');
+            setTimeout(() => menu.classList.add('visible'), 10); // Short delay to trigger transition
+        } else {
+            menu.classList.remove('visible');
+            setTimeout(() => menu.classList.add('hidden'), 500); // Matches transition duration
+        }
+    });
+
+    jobBtnBack.addEventListener('click', () => {
+        jobScroller.scrollBy({ left: -384, behavior: 'smooth' });
+    });
+    jobBtnForward.addEventListener('click', () => {
+        jobScroller.scrollBy({ left: 384, behavior: 'smooth' });
+    });
 });
