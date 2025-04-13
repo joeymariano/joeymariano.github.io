00ac81efad5d30c01078f1cdb9fdc44b1db9eaa8
joeymariano <joey.michalina.mariano@gmail.com>
2025-04-13 12:24:22 -0400

    fixed _ convention for partials

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,9 @@ module.exports = {
     "./_layouts/home.html", // Target your HTML files
     "./_layouts/resume.html", // Target your HTML files
     "./_layouts/post.html", // Target your HTML files
-    "./_includes/footer.html", // Target your HTML files
-    "./_includes/head.html", // Target your HTML files
-    "./_includes/header.html", // Target your HTML files
+    "./_includes/_footer.html", // Target your HTML files
+    "./_includes/_head.html", // Target your HTML files
+    "./_includes/_header.html", // Target your HTML files
     "./assets/js/scripts.js",   // Target your JS files
     // You can add more paths like this if needed
   ],
